b59dff85eabe098fee49407c19fd93af6d2a9352
Ruben <ruben@agrosentinel.com>
2026-04-05 10:52:01 -0300

    Fix report model for Odoo 17

--- l10n_ar_afip/reports/report_models.py
+++ l10n_ar_afip/reports/report_models.py
@@ -4,6 +4,4 @@ from odoo import models
 
 class IrActionsReport(models.Model):
     _inherit = 'ir.actions.report'
-
-    def _render_qweb_pdf(self, report_ref, docids, data, attachment=None, engine='qweb', check_main=True):
-        return super()._render_qweb_pdf(report_ref, docids, data, attachment, engine, check_main)
+    pass
