38dc10ea68d87cb205cd6ff27d2412bfda7a7263
Ruben <ruben@agrosentinel.com>
2026-04-06 11:59:20 -0300

    Disable POS invoice auto-download/open across receipt hooks

--- l10n_ar_ticketarca/static/src/js/receipt_no_invoice_open.js
+++ l10n_ar_ticketarca/static/src/js/receipt_no_invoice_open.js
@@ -4,11 +4,28 @@ import { ReceiptScreen } from "@point_of_sale/app/screens/receipt_screen/receipt
 import { patch } from "@web/core/utils/patch";
 
 patch(ReceiptScreen.prototype, {
+    setup() {
+        super.setup(...arguments);
+        this.l10nArDisableInvoiceOpen = true;
+    },
+
     async _downloadInvoice() {
-        return true;
+        return null;
     },
 
     async downloadInvoice() {
-        return true;
+        return null;
+    },
+
+    async onDownloadInvoice() {
+        return null;
+    },
+
+    _shouldAutoDownloadInvoice() {
+        return false;
+    },
+
+    shouldDownloadInvoice() {
+        return false;
     },
 });
