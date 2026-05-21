a93daade79875575f13c00920ad8174bf035fa29
Ruben <ruben@agrosentinel.com>
2026-05-21 11:23:18 -0300

    feat(theme): add theme_idearte_artistica custom odoo theme

---
+++ theme_idearte_artistica/__manifest__.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Theme Idearte Artística',
+    'description': 'Tema personalizado para Idearte Artística - Tienda de fibrofácil, taller artístico y servicios de Router CNC.',
+    'category': 'Theme/Creative',
+    'version': '19.0.1.0.0',
+    'author': 'Antigravity / AgroSentinel',
+    'website': 'https://idearteartistica.com',
+    'depends': ['website', 'website_sale'],
+    'data': [
+        'views/snippets/hero.xml',
+        'views/snippets/categories.xml',
+        'views/snippets/featured_products.xml',
+        'views/snippets/local_experience.xml',
+        'views/snippets/gallery.xml',
+        'views/snippets/cnc_section.xml',
+        'views/snippets/final_cta.xml',
+        'views/snippets.xml',
+        'views/homepage.xml',
+    ],
+    'assets': {
+        'web.assets_frontend': [
+            'theme_idearte_artistica/static/src/scss/variables.scss',
+            'theme_idearte_artistica/static/src/scss/style.scss',
+        ],
+    },
+    'images': [
+        'static/src/img/logo.png',
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
