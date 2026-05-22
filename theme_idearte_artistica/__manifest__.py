b79bf8861720253f7cd5f182cfa5f8521c5ba3d5
Ruben <ruben@agrosentinel.com>
2026-05-22 09:22:04 -0300

    fix(theme): override add to cart notification button

--- theme_idearte_artistica/__manifest__.py
+++ theme_idearte_artistica/__manifest__.py
@@ -21,6 +21,7 @@
     'assets': {
         'web.assets_frontend': [
             'theme_idearte_artistica/static/src/js/theme_idearte_ui.js',
+            'theme_idearte_artistica/static/src/xml/theme_idearte_notifications.xml',
             'theme_idearte_artistica/static/src/scss/variables.scss',
             'theme_idearte_artistica/static/src/scss/style.scss',
         ],
